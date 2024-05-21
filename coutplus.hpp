00a0825ccdac0f3a2d8dc36751dcf97423666992
kazu-321 <kzs321kzs@gmail.com>
2024-05-21 11:30:59 +0900

    init 変更＆コメント追加

--- coutplus.hpp
+++ coutplus.hpp
@@ -4,12 +4,22 @@
 #include <iostream>
 #include <iomanip>
 
+// initCout(intの表示桁数,flotの表示桁数)
+#define initCout(i,f) cout<<fixed<<setprecision((f))<<left<<setw((i));
+// 画面全削除
 #define clearScreen() cout<<"\033[2"
+// x,yにカーソル移動
 #define setPosition(x,y) cout<<"\033[%"<<(y)+1<<";"<<(x)+1<<"H"
+// カーソル表示オン
 #define cursolOn() cout<<"\033[?25h"
+// カーソル表示オフ
 #define cursolOff() cout<<"\033[?25l"
+// 文字色変更
 #define setCharColor(n) cout<<"\033[3"<<(n)<<"m"
+// 背景色変更
 #define setBackColor(n) cout<<"\033[4"<<(n)<<"m"
+// 文字効果変更
+#define setAttribute(n) cout<<"\033["<<(n)<<"m"
 #define BLACK 0
 #define RED 1
 #define GREEN 2
@@ -19,7 +29,6 @@
 #define CYAN 6
 #define WHITE 7
 #define DEFAULT 9
-#define setAttribute(n) cout<<"\033["<<(n)<<"m"
 #define NORMAL 0 //通常
 #define BLIGHT 1 //明るく
 #define DIM 2 //暗く
@@ -31,7 +40,6 @@
 
 class coutplus {
 public:
-    void init(int int_dig=4,int float_dig=4);
     explicit coutplus(int line=-1);
     template<typename T>
     coutplus &operator<<(const T &value) {
